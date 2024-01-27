b7019f0e49601f087098175a5605ea86dd5985c3
franconil <franconil04@gmail.com>
2024-01-27 01:08:18 +0100

    3primeros puntos del ej5

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,42 @@
+class Tiempo(hora:Int){
+    private var hora = 0
+        set(value) {
+            require(value < 24) { "Un dia no tiene mas de 24 horas" }
+            field = value
+        }
+
+    constructor(hora: Int, min:Int):this(hora){
+        this.min = min
+    }
+
+    constructor(hora: Int, min:Int, seg: Int):this(hora,min ){
+        this.seg = seg
+    }
+
+    private var min = 0
+        set(value) {
+            this.hora += value / 60
+            field = value % 60
+        }
+
+    private var seg = 0
+        set(value) {
+            this.min += value / 60
+            field = value % 60
+        }
+
+    init {
+        this.hora = hora
+    }
+    override fun toString(): String {
+        return "${this.hora}h, ${min}m, ${seg}s"
+    }
+
+}
+
 fun main() {
-    println("Hello World!")
+    val hora = Tiempo(5,73,98)
+    val hora2 = Tiempo(2)
+    println(hora.toString())
+    println(hora2.toString())
 }
